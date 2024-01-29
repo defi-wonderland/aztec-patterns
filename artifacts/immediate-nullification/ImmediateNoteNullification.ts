5b7fa886710692d66a5a8cfbbbcaef057327ee80
0xng <ng@defi.sucks>
2024-01-29 14:08:48 -0300

    fix: contract name

--- artifacts/immediate-nullification/ImmediateNoteNullification.ts
+++ artifacts/immediate-nullification/ImmediateNoteNullification.ts
@@ -23,20 +23,20 @@ import {
   PublicKey,
   Wallet,
 } from '@aztec/aztec.js';
-import SharedNoteContractArtifactJson from '../../target/note_sharing_pattern-SharedNote.json' assert { type: 'json' };
-export const SharedNoteContractArtifact = loadContractArtifact(SharedNoteContractArtifactJson as NoirCompiledContract);
+import ImmediateNoteNullificationContractArtifactJson from '../../target/immediate_note_nullification-ImmediateNoteNullification.json' assert { type: 'json' };
+export const ImmediateNoteNullificationContractArtifact = loadContractArtifact(ImmediateNoteNullificationContractArtifactJson as NoirCompiledContract);
 
 /**
- * Type-safe interface for contract SharedNote;
+ * Type-safe interface for contract ImmediateNoteNullification;
  */
-export class SharedNoteContract extends ContractBase {
+export class ImmediateNoteNullificationContract extends ContractBase {
   
   private constructor(
     completeAddress: CompleteAddress,
     wallet: Wallet,
     portalContract = EthAddress.ZERO
   ) {
-    super(completeAddress, SharedNoteContractArtifact, wallet, portalContract);
+    super(completeAddress, ImmediateNoteNullificationContractArtifact, wallet, portalContract);
   }
   
 
@@ -51,7 +51,7 @@ export class SharedNoteContract extends ContractBase {
     address: AztecAddress,
     wallet: Wallet,
   ) {
-    return Contract.at(address, SharedNoteContract.artifact, wallet) as Promise<SharedNoteContract>;
+    return Contract.at(address, ImmediateNoteNullificationContract.artifact, wallet) as Promise<ImmediateNoteNullificationContract>;
   }
 
   
@@ -59,14 +59,14 @@ export class SharedNoteContract extends ContractBase {
    * Creates a tx to deploy a new instance of this contract.
    */
   public static deploy(wallet: Wallet, ) {
-    return new DeployMethod<SharedNoteContract>(Point.ZERO, wallet, SharedNoteContractArtifact, SharedNoteContract.at, Array.from(arguments).slice(1));
+    return new DeployMethod<ImmediateNoteNullificationContract>(Point.ZERO, wallet, ImmediateNoteNullificationContractArtifact, ImmediateNoteNullificationContract.at, Array.from(arguments).slice(1));
   }
 
   /**
    * Creates a tx to deploy a new instance of this contract using the specified public key to derive the address.
    */
   public static deployWithPublicKey(publicKey: PublicKey, wallet: Wallet, ) {
-    return new DeployMethod<SharedNoteContract>(publicKey, wallet, SharedNoteContractArtifact, SharedNoteContract.at, Array.from(arguments).slice(2));
+    return new DeployMethod<ImmediateNoteNullificationContract>(publicKey, wallet, ImmediateNoteNullificationContractArtifact, ImmediateNoteNullificationContract.at, Array.from(arguments).slice(2));
   }
   
 
@@ -75,23 +75,23 @@ export class SharedNoteContract extends ContractBase {
    * Returns this contract's artifact.
    */
   public static get artifact(): ContractArtifact {
-    return SharedNoteContractArtifact;
+    return ImmediateNoteNullificationContractArtifact;
   }
   
 
   /** Type-safe wrappers for the public methods exposed by the contract. */
   public methods!: {
     
-    /** alice_action(bob: struct) */
-    alice_action: ((bob: AztecAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
-    /** create_and_share_note(bob: struct) */
-    create_and_share_note: ((bob: AztecAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
-    /** bob_action(alice: struct) */
-    bob_action: ((alice: AztecAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** consume_note(randomness: field) */
+    consume_note: ((randomness: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
     /** compute_note_hash_and_nullifier(contract_address: struct, nonce: field, storage_slot: field, preimage: array) */
     compute_note_hash_and_nullifier: ((contract_address: AztecAddressLike, nonce: FieldLike, storage_slot: FieldLike, preimage: FieldLike[]) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** create_note(randomness: field) */
+    create_note: ((randomness: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** are_parameters_nullified(owner: struct, randomness: field) */
+    are_parameters_nullified: ((owner: AztecAddressLike, randomness: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
   };
 }
